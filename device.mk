b77e2a814992e8f92b227dc041f393d1966e5d5e
Hrutvik Jagtap <hrutvikjagtap51@gmail.com>
2020-09-15 18:36:08 +0530

    A6020: Add health hal

--- device.mk
+++ device.mk
@@ -374,3 +374,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.vibrator@1.0-impl \
     android.hardware.vibrator@1.0-service
+
+# Health
+PRODUCT_PACKAGES += \
+    android.hardware.health@2.0-impl \
+    android.hardware.health@2.0-service
