AholudreENA/android_device_lenovo_A6020

A6020: releasetools: Mount/Unmount system from shell
Change-Id: I2272051f3f6050a01553fa3f299d7109774133a7

configs/move_files.sh
@@ -1,4 +1,9 @@
 #!/sbin/sh
+
+echo "Mounting system"
+mkdir /system_root
+mount -t ext4 /dev/block/platform/msm_sdcc.1/by-name/system /system_root -o rw,discard
+
 board_id="";
 for e in $(cat /proc/cmdline);
 do
@@ -29,9 +34,11 @@ case "$board_id" in
 esac
 
 # Move variant-specific blobs
-mv /system/system/etc/firmware/variant/$device/* /system/system/etc/firmware/
-rm -rf /system/system/etc/firmware/variant
+mv /system_root/etc/firmware/variant/$device/* /system_root/etc/firmware/
+rm -rf /system_root/etc/firmware/variant
 
 # Copy media configs
-mv /system/system/vendor/etc/media_codecs_$soc.xml /system/system/vendor/etc/media_codecs.xml
-mv /system/system/vendor/etc/media_codecs_performance_$soc.xml /system/system/vendor/etc/media_codecs_performance.xml
+mv /system_root/vendor/etc/media_codecs_$soc.xml /system_root/vendor/etc/media_codecs.xml
+mv /system_root/vendor/etc/media_codecs_performance_$soc.xml /system_root/vendor/etc/media_codecs_performance.xml
+
+umount /system_root

Filip62/android_device_lenovo_A6020-hsj51

configs/move_files.sh
@@ -1,4 +1,9 @@
 #!/sbin/sh
+
+echo "Mounting system"
+mkdir /system
+mount -t ext4 /dev/block/platform/msm_sdcc.1/by-name/system /system -o rw,discard
+
 board_id="";
 for e in $(cat /proc/cmdline);
 do
@@ -29,9 +34,11 @@ case "$board_id" in
 esac
 
 # Move variant-specific blobs
-mv /system/system/etc/firmware/variant/$device/* /system/system/etc/firmware/
-rm -rf /system/system/etc/firmware/variant
+mv /system/etc/firmware/variant/$device/* /system/etc/firmware/
+rm -rf /system/etc/firmware/variant
 
 # Copy media configs
-mv /system/system/vendor/etc/media_codecs_$soc.xml /system/system/vendor/etc/media_codecs.xml
-mv /system/system/vendor/etc/media_codecs_performance_$soc.xml /system/system/vendor/etc/media_codecs_performance.xml
+mv /system/vendor/etc/media_codecs_$soc.xml /system/vendor/etc/media_codecs.xml
+mv /system/vendor/etc/media_codecs_performance_$soc.xml /system/vendor/etc/media_codecs_performance.xml
+
+umount /system